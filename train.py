0e1a290482e98b3bb0790576dfab97ea3df8a77e
qwertyu0 <91277172+qwertyu0@users.noreply.github.com>
2026-05-06 23:44:42 +0300

    Update train.py
    бд

--- train.py
+++ train.py
@@ -1,21 +1,49 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
-with open("laws/uk.txt", "r", encoding="utf-8") as f:
-    text = f.read()
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-chunks = splitter.split_text(text)
+
+import os
 
 embedding = HuggingFaceEmbeddings(
     model_name="sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 )
-db = Chroma.from_texts(
-    chunks,
-    embedding,
-    persist_directory="db"
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
 )
-db.persist()
-print("готово")
+
+# =========================
+# ФУНКЦИЯ СОЗДАНИЯ БАЗЫ
+# =========================
+def build_db(folder, db_name):
+    texts = []
+
+    for file in os.listdir(folder):
+        if file.endswith(".txt"):
+            path = os.path.join(folder, file)
+
+            with open(path, "r", encoding="utf-8") as f:
+                texts.append(f.read())
+
+    full_text = "\n\n".join(texts)
+
+    chunks = splitter.split_text(full_text)
+
+    print(f"{db_name}: чанков {len(chunks)}")
+
+    db = Chroma.from_texts(
+        chunks,
+        embedding,
+        persist_directory=db_name
+    )
+
+    db.persist()
+
+    print(f"{db_name} готова")
+
+
+build_db("laws/uk", "db_uk")
+build_db("laws/koap", "db_koap")
+build_db("laws/fz", "db_fz")
